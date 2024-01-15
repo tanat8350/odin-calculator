f20332ff1916883b419948e66255b6cf062667eb
Tanat <55195958+tanat8350@users.noreply.github.com>
2024-01-15 08:31:36 +0700

    Revert "goes wrong"
    This reverts commit 918603e6c64f80e071d5249601270775420a05ae.

--- script.js
+++ script.js
@@ -27,26 +27,9 @@ function operate(num1, num2, operator) {
 
 const calculatorP = document.querySelector("#calculator-p");
 
-let store = "";
-
 const buttonContainer = document.querySelector("#button-container");
-// buttonContainer.addEventListener("click", (e) => {
-//   const target = e.target;
-
-//   if (target.textContent === "=") {
-//     operate2(calculatorP.textContent);
-//   } else {
-//     calculatorP.textContent += target.textContent;
-//   }
-// });
-
 buttonContainer.addEventListener("click", (e) => {
   const target = e.target;
-
-  if (target.textContent === " + ") {
-    store = operate2(store);
-    store += target.textContent;
-  }
 
   if (target.textContent === "=") {
     operate2(calculatorP.textContent);
@@ -56,19 +39,12 @@ buttonContainer.addEventListener("click", (e) => {
 });
 
 function operate2(string) {
-  const splited = string.split(" ");
-  if (splited.length < 2) {
-    return;
-  } else {
-    let [num1, operator, num2] = string.split(" ");
-    num1 = +num1;
-    num2 = +num2;
+  let [num1, operator, num2] = string.split(" ");
+  num1 = +num1;
+  num2 = +num2;
 
-    operator === "+" ? (store = add(+num1, +num2)) : "";
-    operator === "-" ? (calculatorP.textContent = subtract(num1, num2)) : "";
-    operator === "*" ? (calculatorP.textContent = multiply(num1, num2)) : "";
-    operator === "/" ? (calculatorP.textContent = divide(num1, num2)) : "";
-
-    calculatorP.textContent = store;
-  }
+  operator === "+" ? (calculatorP.textContent = add(num1, num2)) : "";
+  operator === "-" ? (calculatorP.textContent = subtract(num1, num2)) : "";
+  operator === "*" ? (calculatorP.textContent = multiply(num1, num2)) : "";
+  operator === "/" ? (calculatorP.textContent = divide(num1, num2)) : "";
 }
