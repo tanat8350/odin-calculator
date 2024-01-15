363559f121a80d756d70159f6505a2196464ec58
Tanat <55195958+tanat8350@users.noreply.github.com>
2024-01-15 09:50:13 +0700

    Done without UI

--- script.js
+++ script.js
@@ -1,50 +1,63 @@
-const num1 = 1;
-const num2 = 2;
-const operator = "+";
-
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
+let num1 = 0;
+let num2 = 0;
+let operator = "";
 
 function operate(num1, num2, operator) {
-  operator === "+" ? add(num1, num2) : "";
-  operator === "-" ? subtract(num1, num2) : "";
-  operator === "*" ? multiply(num1, num2) : "";
-  operator === "/" ? divide(num1, num2) : "";
+  let result = 0;
+  operator === "+" ? (result = num1 + num2) : "";
+  operator === "-" ? (result = num1 - num2) : "";
+  operator === "*" ? (result = num1 * num2) : "";
+  operator === "/" ? (result = num1 / num2) : "";
+  !operator ? (result = num2) : "";
+  calDisplay.textContent = result;
+  calDisplay.classList.add("js-result");
+  return result;
 }
 
-const calculatorP = document.querySelector("#calculator-p");
+const calDisplay = document.querySelector("#cal-display");
+const calLatest = document.querySelector("#cal-latest");
 
 const buttonContainer = document.querySelector("#button-container");
 buttonContainer.addEventListener("click", (e) => {
   const target = e.target;
 
-  if (target.textContent === "=") {
-    operate2(calculatorP.textContent);
+  if (target.id === "clear-btn") {
+    calDisplay.textContent = "";
+    calLatest.textContent = "";
+    num1 = num2 = 0;
+    operator = "";
+  } else if (target.id === "del-btn") {
+    calDisplay.textContent = calDisplay.textContent.slice(0, -1);
+  } else if (target.classList.contains("js-equal")) {
+    num2 = +calDisplay.textContent;
+    if (operator) {
+      calLatest.textContent = `${num1} ${operator} ${num2} =`;
+    } else {
+      calLatest.textContent = `= ${num2}`;
+    }
+    operate(num1, num2, operator);
+    num1 = +calDisplay.textContent;
+    operator = "";
+  } else if (target.classList.contains("js-operator")) {
+    if (operator === "") {
+      num1 = +calDisplay.textContent;
+      operator = target.textContent;
+      calLatest.textContent = `${num1} ${operator}`;
+      calDisplay.classList.add("js-result");
+    } else {
+      num2 = +calDisplay.textContent;
+      num1 = operate(num1, num2, operator);
+      operator = target.textContent;
+      calLatest.textContent = `${num1} ${operator}`;
+    }
+  } else if (target.classList.contains("js-decimal")) {
+    if (!calDisplay.textContent.includes(".")) {
+      calDisplay.textContent += target.textContent;
+    }
+  } else if (calDisplay.classList.contains("js-result")) {
+    calDisplay.textContent = target.textContent;
+    calDisplay.classList.remove("js-result");
   } else {
-    calculatorP.textContent += target.textContent;
+    calDisplay.textContent += target.textContent;
   }
 });
-
-function operate2(string) {
-  let [num1, operator, num2] = string.split(" ");
-  num1 = +num1;
-  num2 = +num2;
-
-  operator === "+" ? (calculatorP.textContent = add(num1, num2)) : "";
-  operator === "-" ? (calculatorP.textContent = subtract(num1, num2)) : "";
-  operator === "*" ? (calculatorP.textContent = multiply(num1, num2)) : "";
-  operator === "/" ? (calculatorP.textContent = divide(num1, num2)) : "";
-}
